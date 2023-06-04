cade8b12b6296a045e47cfe5a334cd7eaab50749
Tony <qkrdkwl9090@naver.com>
2023-06-04 20:47:23 +0900

    feat(layout): modify default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,30 +3,16 @@ import ScrollTop from 'primevue/scrolltop'
 </script>
 
 <template>
-  <div class="h-screen | overflow-x-hidden">
+  <div class="main__container overflow-x-hidden shrink-0">
     <Header />
-    <div
-      class="main__container with-header | w-screen overflow-x-hidden shrink-0"
-    >
-      <main><slot /></main>
-      <ScrollTop target="parent" />
-    </div>
+    <main><slot /></main>
+    <ScrollTop target="parent" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-$header-height: 80px;
-$footer-height: 100px;
-.main__container {
-  overflow-y: overlay;
-  &.with-header {
-    margin-top: $header-height;
-    height: calc(100vh - #{$header-height});
-    height: calc(100dvh - #{$header-height});
-  }
-}
 main {
-  min-height: calc(100vh - #{$header-height} - #{$footer-height});
-  min-height: calc(100dvh - #{$header-height} - #{$footer-height});
+  min-height: 100vh;
+  min-height: 100vdh;
 }
 </style>
